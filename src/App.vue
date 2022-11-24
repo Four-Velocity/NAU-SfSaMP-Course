40668fcceb459d45994c7e76ddf7b2a289ad6f62
Oleh Mazur <oleh@four-velocity.com>
2022-11-24 01:47:49 +0100

    :tada: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <AppBar />
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import AppBar from '@/components/AppBar.vue'
+</script>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-app-bar color="primary" density="compact">
+    <v-app-bar-title>Лабораторна робота №1</v-app-bar-title>
+  </v-app-bar>
+</template>
