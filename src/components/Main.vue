1dd119e7b309db366b03f20f3fca2df3c7785921
Oleh Mazur <oleh@four-velocity.com>
2022-11-24 03:52:05 +0100

    :two: Task two completed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-toolbar>
+  <v-toolbar color="primary">
     <v-toolbar-title> Лабораторна робота №1 </v-toolbar-title>
 
     <template v-slot:extension>
-      <v-tabs v-model="tab" fixed-tabs>
+      <v-tabs v-model="tab" fixed-tabs color="secondary">
         <v-tab value="one">Завдання №1</v-tab>
+        <v-tab value="two">Завдання №2</v-tab>
       </v-tabs>
     </template>
   </v-toolbar>
@@ -13,11 +14,16 @@
     <v-window-item value="one">
       <Task1 />
     </v-window-item>
+
+    <v-window-item value="two">
+      <Task2 />
+    </v-window-item>
   </v-window>
 </template>
 
 <script>
 import Task1 from "@/components/Task1.vue";
+import Task2 from "@/components/Task2.vue";
 
 export default {
   data: () => ({
@@ -25,6 +31,7 @@ export default {
   }),
   components: {
     Task1,
+    Task2,
   },
 };
 </script>
---
+++ src/components/Task2.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-card-text> Олег Мазур </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+.v-card-text {
+  font-style: italic;
+  font-size: 23px;
+  color: rgb(255, 204, 0);
+}
+</style>
