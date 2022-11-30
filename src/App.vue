7361288648a7a70d0fbe4a39e743449bd15e6da1
Oleh Mazur <oleh@four-velocity.com>
2022-11-30 03:13:56 +0100

    :fire: Remove old PWA setup

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,10 @@
   <v-app>
     <v-main>
       <Main></Main>
-      <ReloadPrompt />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
   import Main from '@/components/Main.vue'
-  import ReloadPrompt from './ReloadPrompt.vue'
 </script>
